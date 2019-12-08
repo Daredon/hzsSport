04f3bd8f0029c124c582a96a34b0b0d827117ad0
Nemanja02 <nemanja02marjanov@gmail.com>
2019-12-08 07:26:41 +0100

    ok

--- app/Groups.php
+++ app/Groups.php
@@ -9,7 +9,8 @@ class Groups
 
     public static function getUserGroup() {
         if (session()->get('id') !== null) {
-            return DB::table('users')->select('id', 'username', 'group_id')->where('group_id', '=', session()->get('id'))->get();
+            $grp_id = DB::table('users')->select('group_id')->where('id', '=', session()->get('id'))->get()[0]->group_id;
+            return DB::table('users')->select('id', 'username', 'group_id')->where('group_id', '=', $grp_id);
         } else {
             return null;
         }
